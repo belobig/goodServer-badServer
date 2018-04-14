2c1639cd53e19302d79afccf9020ebd2296d9c7d
Kelly Costner <32380985+belobig@users.noreply.github.com>
2018-04-14 10:24:08 -0600

    Separated my sides

---
+++ justBad.js
@@ -0,0 +1,21 @@
+// Require/import the HTTP module
+var http = require("http");
+
+var BADPORT = 7500;
+
+// Create a generic function to handle requests and responses
+function badHandleRequest(request, response) {
+
+	// Send the below string to the client when the user visits the PORT URL
+	response.end("You Suck!! Path Hit: " + request.url);
+
+}
+
+var badServer = http.createServer(badHandleRequest);
+
+// Start our server so that it can begin listening to client requests.
+badServer.listen(BADPORT, function () {
+
+	// Log (server-side) when our server has started
+	console.log("Bad Server listening on: http://localhost:" + BADPORT);
+});
